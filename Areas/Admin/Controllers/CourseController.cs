caa4ff7f279601d737b35ba862ba996622de3487
sonhale57 <sonhale57@gmail.com>
2025-04-11 16:57:17 +0700

    update giao diện user

---
+++ Areas/Admin/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace tuoitrevanduc.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseController : Controller
+    {
+        [Route("/admin/khoa-tu")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/admin/loai-khoa-tu")]
+        public IActionResult CategoryIndex()
+        {
+            return View();
+        }
+    }
+}
